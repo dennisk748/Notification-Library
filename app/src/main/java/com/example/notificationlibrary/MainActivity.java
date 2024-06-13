6cbcf389f6f15e16afbce9c7256565a4f57ed343
Master Luke Otieno <lukemclain4@gmail.com>
2024-06-13 20:16:35 +0300

    Defined the textView component

--- app/src/main/java/com/example/notificationlibrary/MainActivity.java
+++ app/src/main/java/com/example/notificationlibrary/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.notificationlibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtView = findViewById(R.id.txtView);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
